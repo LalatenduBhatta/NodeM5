f775ec3148e69ad6634791c70d9cd7441fc1bc1e
LalatenduBhatta <lalatendubhatta@gmail.com>
2024-03-23 13:30:59 +0530

    crud operation

--- postman/index.js
+++ postman/index.js
@@ -13,8 +13,19 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("HI FROM THE SERVER")
 })
-app.get("/getdata", (req, res) => {
-    res.json({ name: "Messi", age: 36, address: "ARGENTINA" })
+app.get("/getdata", async (req, res) => {
+    let data = await userModel.find()
+    res.status(200).send(data)
+})
+app.get("/getuser/:name", async (req, res) => {
+    // console.log(req.params);
+    const { name } = req.params
+    let user = await userModel.findOne({ name: name })
+    if (user) {
+        res.status(200).send({ user })
+    } else {
+        res.status(404).send({ message: "user not found" })
+    }
 })
 //post API
 app.post("/newuser", async (req, res) => {
@@ -22,6 +33,30 @@ app.post("/newuser", async (req, res) => {
     let user = new userModel(req.body)
     let response = await user.save()
     res.send("data stored in db")
+})
+
+//deleteAPI
+app.delete("/deleteuser", async (req, res) => {
+    // console.log(req.query);
+    const { name } = req.query
+    let response = await userModel.deleteOne({ name: name })
+    // console.log(response);
+    if (response.deletedCount > 0) {
+        res.status(200).send({ message: "user deleted successfully" })
+    } else {
+        res.status(400).send({ message: "user not registered" })
+    }
+})
+//updateAPI
+app.put("/updateuser/:name", async (req, res) => {
+    const { name } = req.params
+    const updateData = req.body
+    const response = await userModel.updateOne({ name: name }, {
+        $set: {
+            ...updateData
+        }
+    })
+    res.send(response)
 })
 
 app.listen(PORT, hostName, () => {
--- postman/model/userModel.js
+++ postman/model/userModel.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const userSchema = mongoose.Schema({
     name: { type: String },
     age: { type: String },
-    address: { type: String }
+    address: { type: String },
+    mobile: { type: Number }
 })
 
 const userModel = mongoose.model("users", userSchema)
